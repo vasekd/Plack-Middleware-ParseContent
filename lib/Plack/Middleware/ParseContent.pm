215cbeaa5720ee33be422fb026d85cc995888eb8
Václav Dovrtěl <vaclav.dovrtel@gmail.com>
2015-04-08 08:37:36 +0200

    0.04

--- lib/Plack/Middleware/ParseContent.pm
+++ lib/Plack/Middleware/ParseContent.pm
@@ -4,7 +4,7 @@ use 5.006;
 use strict;
 use warnings FATAL => 'all';
 
-our $VERSION = '0.03'; # Set automatically by milla
+our $VERSION = '0.04'; # Set automatically by milla
 
 use parent qw( Plack::Middleware );
 
